62525f2049c00f602be26091f456ebfe52eeba28
arungautam11 <asgautam19@gmail.com>
2023-10-18 12:32:11 +0530

    remove DEBUG config update with env false

--- blog_main/settings_rail.py
+++ blog_main/settings_rail.py
@@ -5,8 +5,7 @@ import os
 
 SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = ['django-blog-main-production.up.railway.app', '127.0.0.1']
 

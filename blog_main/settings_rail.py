d8f9468cd70f01e6c73747c268d964f0ef13ec26
arungautam11 <asgautam19@gmail.com>
2023-10-18 11:50:36 +0530

    added new databses config

--- blog_main/settings_rail.py
+++ blog_main/settings_rail.py
@@ -2,6 +2,19 @@ from blog_main.settings import *
 
 from decouple import config
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY') 
 
-ALLOWED_HOSTS = ['django-blog-main-production.up.railway.app']
+ALLOWED_HOSTS = ['django-blog-main-production.up.railway.app', '127.0.0.1']
+
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DATABASE_NAME'),
+            'USER': config('DATABASE_USER'),
+            'PASSWORD': config('DATABASE_PASSWORD'),
+            'HOST': config('DATABASE_HOST'),
+            'PORT': config('DATABASE_PORT'),
+            'OPTIONS': {'sslmode':'require'},
+        }
+    }
